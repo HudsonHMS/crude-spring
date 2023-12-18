548330de7256ad6305b6ece65b7aac33f37194f7
hudson.marques <hyliam30@gmail.com>
2023-12-18 19:36:02 -0300

    Inicio do projeto

---
+++ src/main/java/com/hms/crude_spring/crudespring/CrudeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.hms.crude_spring.crudespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudeSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hms/crude_spring/crudespring/CrudeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hms.crude_spring.crudespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
